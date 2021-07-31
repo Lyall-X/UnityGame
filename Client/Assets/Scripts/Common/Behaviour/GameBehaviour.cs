8d14539228e890b52f15a9e9de3da9b5a9ad2857
liuyue <liuyue@xd.com>
2021-07-31 13:06:44 +0800

    初始化unity客户端

---
+++ Client/Assets/Scripts/Common/Behaviour/GameBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameBehaviour : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+}
