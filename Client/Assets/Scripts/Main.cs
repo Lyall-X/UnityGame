d6341fbbec9e87f2b82dc2ea8859cd0934750193
liuyue <liuyue@xd.com>
2021-07-31 14:20:03 +0800

    添加vscode调试方案

--- Client/Assets/Scripts/Main.cs
+++ Client/Assets/Scripts/Main.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("aaaaa");
     }
 }
