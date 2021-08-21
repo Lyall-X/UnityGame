6d73b3906422178f1a3e3b2d2146a470304e1495
liuyue <liuyue@xd.com>
2021-08-21 14:51:27 +0800

    提交

--- Client/Assets/Editor/FixChecker.cs
+++ Client/Assets/Editor/FixChecker.cs
@@ -2,9 +2,27 @@
 using UnityEditor;
 using System.IO;
 using System.Text;
+using Client.Component;
 
 public class FixChecker : BaseEditor
 {
+    [MenuItem ("FixChecker/替换所有的文本框组件")]
+    static void ReplaceAllText () 
+    {
+        var path = "Assets/GameObject.prefab";
+        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+        var gameObj = GameObject.Instantiate<GameObject>(prefab);
+        var components = gameObj.transform.GetComponentsInChildren<CText>();
+        for (int i = 0; i < components.Length; i++) 
+        {
+            components[i].guid = System.Guid.NewGuid().ToString();
+            Debug.Log(components[i].guid + " " + components[i]);
+        }
+        PrefabUtility.SaveAsPrefabAsset(gameObj, path);
+        AssetDatabase.Refresh();
+    }
+
     static string emptyAssetPath = "Assets/Resources/Textures/Empty.png";
     static string uiAssetPath = "Assets/res/Prefabs/UI/";
 
